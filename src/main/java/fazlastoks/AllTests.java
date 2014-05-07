264d58fc0eda51cf334cfd18e04a83ba611edb58
hebun <itung73@gmail.com>
2014-05-07 13:30:52 +0300

    adding mastertest unit

---
+++ src/main/java/fazlastoks/AllTests.java
@@ -0,0 +1,11 @@
+package fazlastoks;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ MasterTest.class, TestPro.class })
+public class AllTests {
+
+}
---
+++ src/main/java/fazlastoks/MasterTest.java
@@ -0,0 +1,50 @@
+package fazlastoks;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class MasterTest {
+
+	static Master master;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		master = new Master();
+		assertNotNull(master.getCats());
+		assertTrue("size is 0", master.getCats().size() > 0);
+
+	}
+
+	@Test
+	public void testSearch() {
+		master.setSearchText("x");
+		String ret = master.search();
+		assertNull(ret);
+		master.setSearchText("test");
+		ret = master.search();
+		assertEquals("urun-arama?faces-redirect=true&key=test", ret);
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testMaster() {
+
+	}
+
+}
